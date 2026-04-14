217d7f749f42cc01a6a2a56a67c2f87e20b26fb0
budi utomo <budtmo.os@gmail.com>
2026-04-14 06:30:01 +0200

    Added lcd and implemented button numbers handle separately

--- mp3Player/Mp3Player.ino
+++ mp3Player/Mp3Player.ino
@@ -1,13 +1,56 @@
 #include "SoftwareSerial.h"
 #include "DFRobotDFPlayerMini.h"
 #include "IRremote.h"
+#include <LiquidCrystal.h>
 
-SoftwareSerial mySoftwareSerial(3, 4); // RX, TX
+SoftwareSerial mySoftwareSerial(10, 11); // RX, TX
 DFRobotDFPlayerMini myDFPlayer;
-int receiverPin = 2;
+int receiverPin = 13;
 IRrecv irrecv(receiverPin);
+LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 
 bool isPaused = false;
+int8_t currentTrackIndex = -1; // -1 means no track, in case user press next for the first time.
+
+String displayedText = "LCD is ready";
+
+int currentVolume = 15;
+bool showingVolume = false;
+unsigned long volumeDisplayStart = 0;
+const unsigned long VOLUME_DISPLAY_DURATION = 3000; // ms
+
+struct TrackEntry {
+  uint32_t   code;
+  uint8_t    track;
+  const char* name;
+};
+
+const TrackEntry tracks[] = {
+  {0xF30CFF00,  1, "01.light of the world"},
+  {0xE718FF00,  2, "02.ja, so ist mein Gott"},
+  {0xA15EFF00,  3, "03.mein fels"},
+  {0xF708FF00,  4, "04.f\xFCrchte dich nicht"},
+  {0xE31CFF00,  5, "05.lalalass dich nicht"},
+  {0xA55AFF00,  6, "06.liebt einander"},
+  {0xBD42FF00,  7, "07.nichts unm\xF6glich"},
+  {0xAD52FF00,  8, "08.top top top wichtig"},
+  {0xB54AFF00,  9, "09.Du gibst frieden"},
+  {0xE916FF00, 10, "10.freude freunde"},
+};
+const uint8_t NUM_TRACKS = sizeof(tracks) / sizeof(tracks[0]);
+
+void printLCD(String text) {
+  lcd.clear();
+  if (text.length() <= 16) {
+    lcd.setCursor(0, 0);
+    lcd.print(text);
+  } else {
+    lcd.setCursor(0, 0);
+    lcd.print(text.substring(0, 16));
+    lcd.setCursor(0, 1);
+    lcd.print(text.substring(16));
+  }
+}
 
 void setup() {
   mySoftwareSerial.begin(9600);
@@ -20,112 +63,117 @@ void setup() {
     while(true); //infinite loop to stop program from running for safety
   }
 
-  myDFPlayer.volume(5);  //Set volume value. From 0 to 30
+  myDFPlayer.volume(15);  //Set volume value. From 0 to 30
   Serial.println("DFPlayer Ready");
 
   IrReceiver.begin(receiverPin, ENABLE_LED_FEEDBACK);
   Serial.println("IR Receiver Ready");
+
+  lcd.begin(16, 2); // Coloum, Row
+  lcd.setCursor(0, 0);
+  lcd.print(displayedText);
 }
 
 void loop()
 {
+  // Revert volume display back to song name after timeout
+  if (showingVolume && (millis() - volumeDisplayStart >= VOLUME_DISPLAY_DURATION)) {
+    showingVolume = false;
+    printLCD(displayedText);
+  }
+
   if (IrReceiver.decode()) {
     uint32_t code = IrReceiver.decodedIRData.decodedRawData;
     Serial.print("IR Code: 0x");
     Serial.println(code, HEX);
-    switch (code) {
-      case 0xF30CFF00: // Button 1
-        Serial.println("Play track 1");
-        myDFPlayer.play(1);
-        break;
 
-      case 0xE718FF00: // Button 2
-        Serial.println("Play track 2");
-        myDFPlayer.play(2);
-        break;
+    bool handled = false;
 
-      case 0xA15EFF00: // Button 3
-        Serial.println("Play track 3");
-        myDFPlayer.play(3);
+    // Check track buttons via lookup table (easy to extend: just add a row)
+    for (uint8_t i = 0; i < NUM_TRACKS; i++) {
+      if (code == tracks[i].code) {
+        Serial.print("Play track ");
+        Serial.println(tracks[i].track);
+        currentTrackIndex = i;
+        displayedText = tracks[i].name;
+        myDFPlayer.play(tracks[i].track);
+        showingVolume = false;
+        printLCD(displayedText);
+        handled = true;
         break;
-
-      case 0xF708FF00: // Button 4
-        Serial.println("Play track 4");
-        myDFPlayer.play(4);
-        break;
-
-      case 0xE31CFF00: // Button 5
-        Serial.println("Play track 5");
-        myDFPlayer.play(5);
-        break;
-
-      case 0xA55AFF00: // Button 6
-        Serial.println("Play track 6");
-        myDFPlayer.play(6);
-        break;
-
-      case 0xBD42FF00: // Button 7
-        Serial.println("Play track 7");
-        myDFPlayer.play(7);
-        break;
-
-      case 0xAD52FF00: // Button 8
-        Serial.println("Play track 8");
-        myDFPlayer.play(8);
-        break;
-
-      case 0xB54AFF00: // Button 9
-        Serial.println("Play track 9");
-        myDFPlayer.play(9);
-        break;
-
-      case 0xE916FF00: // Button 0
-        Serial.println("Play track 10");
-        myDFPlayer.play(10);
-        break;
-
-      case 0xBB44FF00: // Button Fast Back
-        Serial.println("Previous Play");
-        myDFPlayer.previous();
-        break;
-
-      case 0xBC43FF00: // Button Fast Forward
-        Serial.println("Next Play");
-        myDFPlayer.next();
-        break;
-
-      case 0xBF40FF00: // Button Pause/Resume
-        if (!isPaused) {
-          Serial.println("Pause");
-          myDFPlayer.pause();
-          isPaused = true;
-        } else {
-          Serial.println("Resume");
-          myDFPlayer.start();
-          isPaused = false;
-        }
-        break;
-
-      case 0xB847FF00: // Button Func/Stop
-        Serial.println("Stop");
-        myDFPlayer.stop();
-        break;
-
-      case 0xB946FF00: // VOL+
-        Serial.println("Volume Up");
-        myDFPlayer.volumeUp();   // increases current volume by 1
-        break;
-
-      case 0xEA15FF00: // VOL-
-        Serial.println("Volume Down");
-        myDFPlayer.volumeDown(); // decreases current volume by 1
-        break;
-
-      default:
-        Serial.println("Other button");
-        break;
+      }
     }
-    delay(300); // Do not get immediate repeat and avoid double click
+
+    if (!handled) {
+      switch (code) {
+        case 0xBB44FF00: // Button Fast Back
+          Serial.println("Previous Play");
+          if (currentTrackIndex <= 0) currentTrackIndex = NUM_TRACKS - 1;
+          else currentTrackIndex--;
+          displayedText = tracks[currentTrackIndex].name;
+          myDFPlayer.previous();
+          showingVolume = false;
+          printLCD(displayedText);
+          break;
+
+        case 0xBC43FF00: // Button Fast Forward
+          Serial.println("Next Play");
+          if (currentTrackIndex == -1) currentTrackIndex = 0; // it will play first song
+          else currentTrackIndex = (currentTrackIndex + 1) % NUM_TRACKS;
+          displayedText = tracks[currentTrackIndex].name;
+          myDFPlayer.next();
+          showingVolume = false;
+          printLCD(displayedText);
+          break;
+
+        case 0xBF40FF00: // Button Pause/Resume
+          if (!isPaused) {
+            Serial.println("Pause");
+            myDFPlayer.pause();
+            isPaused = true;
+            showingVolume = false;
+            printLCD("Paused");
+          } else {
+            Serial.println("Resume");
+            myDFPlayer.start();
+            isPaused = false;
+            showingVolume = false;
+            printLCD(displayedText); // show current song on resume
+          }
+          break;
+
+        case 0xB847FF00: // Button Func/Stop
+          Serial.println("Stop");
+          myDFPlayer.stop();
+          showingVolume = false;
+          printLCD("Stopped");
+          break;
+
+        case 0xB946FF00: // VOL+
+          Serial.println("Volume Up");
+          myDFPlayer.volumeUp();
+          if (currentVolume < 30) currentVolume++;
+          showingVolume = true;
+          volumeDisplayStart = millis();
+          printLCD("Volume: " + String(currentVolume));
+          break;
+
+        case 0xEA15FF00: // VOL-
+          Serial.println("Volume Down");
+          myDFPlayer.volumeDown();
+          if (currentVolume > 0) currentVolume--;
+          showingVolume = true;
+          volumeDisplayStart = millis();
+          printLCD("Volume: " + String(currentVolume));
+          break;
+
+        default:
+          Serial.println("Other button");
+          break;
+      }
+    }
+
+    delay(300); // Avoid immediate repeat / double click
     IrReceiver.resume(); // Ready for next code
   }
 }
